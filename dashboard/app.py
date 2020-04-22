f5d8f33416c9f490d8778e4eb769ad3768df0e26
Andre van Turnhout <andre.vanturnhout@viqtordavis.com>
2020-04-22 11:13:28 +0200

    test commit hooks DAT-2647

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,10 +9,9 @@ from google.cloud import kms_v1
 from authentication.azure_auth import AzureOAuth
 from flask_caching import Cache
 from flask_sslify import SSLify
+from flaks_cors import CORS
 
 server = flask.Flask(__name__)
-if 'GAE_INSTANCE' in os.environ:
-    SSLify(server, permanent=True)
 
 app = dash.Dash(
     __name__,
@@ -20,6 +19,12 @@ app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     server=server,
 )
+if 'GAE_INSTANCE' in os.environ:
+    CORS(app.app, origins=config.ORIGINS)
+    SSLify(server, permanent=True)
+else:
+    CORS(app.app)
+
 cache = Cache(app.server, config={
     "CACHE_TYPE": "simple",
     "CACHE_DEFAULT_TIMEOUT": 300
