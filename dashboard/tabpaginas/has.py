c4af94845c8509b5fa09e3db395ffda2d33ecd29
m.bruisten <m.bruisten@vwt.digital>
2021-05-25 15:04:42 +0200

    DAN-1278: updated style issues

--- dashboard/tabpaginas/has.py
+++ dashboard/tabpaginas/has.py
@@ -437,7 +437,7 @@ def download_excel1_h():
 
 
 # HELPER FUNCTIES
-@cache.memoize()
+@cache.memoize()  # noqa C901
 def data_from_DB(preset_selectie, filter_selectie, category):
     if (not preset_selectie == []) & (not filter_selectie == []):
 
@@ -535,7 +535,7 @@ def generate_graph(OHW, pOHW, donut, df_table, category):
                 textinfo="value",
                 hole=0.5,
                 marker=dict(colors=['#003f5c', '#374c80', '#7a5195',
-                                    '#bc5090',  '#ef5675']),
+                                    '#bc5090', '#ef5675']),
                 domain={"x": [0, 1], "y": [0.30, 1]},
                 sort=False
             )
